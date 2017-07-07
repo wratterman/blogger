71220614d81cca22d915aff2e9973ebddf017dc1
wratterman <will.ratterman@gmail.com>
2017-07-07 13:42:03 -0600

    Set up /articles route and action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,5 @@
 class ArticlesController < ApplicationController
+
   def index
-    render :text => "S/O to the homies Jim and Killa Mike!"
-  end
-
-  def create
-  end
-
-  def new
-    render :html => "You are creating a new task"
-  end
-
-  def show
-    render :html => "You are viewing task #{params[:id]}"
-  end
-
-  def edit
-    render :html => "You are editing task #{params[:id]}"
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+    @articles = Article.all
 end
