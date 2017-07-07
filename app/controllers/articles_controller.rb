33bfdcd6b47f5679f93cc16af08cc4ee0e21ead4
wratterman <will.ratterman@gmail.com>
2017-07-07 14:40:16 -0600

    delete/destroy functionality built and removing from database if selected

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,13 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to '/'
+  end
+
   private
 
   def article_params
