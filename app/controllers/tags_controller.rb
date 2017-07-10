e02105268495492857e5b224c8158e0a1306a6e0
wratterman <will.ratterman@gmail.com>
2017-07-09 20:47:57 -0600

    iteration 5 complete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,20 @@
 class TagsController < ApplicationController
 
+  before_filter :require_login, only: [:destroy]
+
   def index
     @tags = Tag.all
   end
 
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    redirect_to '/'
   end
 end
